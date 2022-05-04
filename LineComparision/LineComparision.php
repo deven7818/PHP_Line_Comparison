fb54fabe3fcb69fd77f6b4e20c2549d1323d29e8
deven7818 <deven7818@gmail.com>
2022-05-04 13:40:52 +0530

    Checking two lines are equals based on end points

--- LineComparision/LineComparision.php
+++ LineComparision/LineComparision.php
@@ -11,7 +11,7 @@ class LineComparision
     //function for welcome message
     function welcome()
     {
-        echo "Welcome to Line Comparison Computation Program";
+        echo "Welcome to Line Comparison Computation Program\n";
     }
 
     //declaring variables
@@ -23,10 +23,10 @@ class LineComparision
     //function to get user input values
     function userInput()
     {
-        $this->x1 = readline("\nEnter Value for x1 : ");
-        $this->y1 = readline("\nEnter Value for y1 : ");
-        $this->x2 = readline("\nEnter Value for x2 : ");
-        $this->y2 = readline("\nEnter Value for y2 : ");
+        $this->x1 = readline("Enter Value for x1 : ");
+        $this->y1 = readline("Enter Value for y1 : ");
+        $this->x2 = readline("Enter Value for x2 : ");
+        $this->y2 = readline("Enter Value for y2 : ");
     }
 
     //function to calculate length from the formula
@@ -36,11 +36,30 @@ class LineComparision
         $this->val1 = pow(($this->x2 - $this->x1), 2);
         $this->val2 = pow(($this->y2 - $this->y1), 2);
         $this->result = sqrt($this->val1 + $this->val2);
-        echo " result is" . $this->result;
+        echo "\nresult is : " . $this->result;
+
+    }
+
+    function lineCompare(){
+         echo "\nEnter values for line 1";
+         $this->line1 = $this->userInput();
+
+        echo "\nEnter values for line - 2 :\n";
+        $this->line2 = $this->userInput();
+
+        if($this->line1 == $this->line2){
+            echo "\nBoth lines are euqals";
+        }else{
+            echo "\nLines are not equals";
+        }
+
+        
     }
 }
 
 $line = new LineComparision();
 $line->welcome();
 echo $line->calculateLength();
+
+$line->lineCompare();
 ?>
